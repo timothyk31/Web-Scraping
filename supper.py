1a410772fcff261e2e18f6cc37990bf3bad51cd3
timothyk31 <timothykurniawan16@gmail.com>
2023-03-28 21:57:47 -0500

    Scraped, edit the strings

--- supper.py
+++ supper.py
@@ -1,7 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-html_text = requests.get('https://engineering.tamu.edu/cse/profiles/index.html#Faculty').text
+html_text = requests.get('https://jazzfuel.com/best-jazz-albums/').text
 soup = BeautifulSoup(html_text, 'lxml')
-desc = soup.find_all('div', class_ = 'profile')
+desc = soup.find_all('b')
 results = ''
+for i in range (0,len(desc)-1,2):
+    tempStr = str(desc[i])
+    if ('<a href') in tempStr:
+        tempStr = str(int(50-i/2)) + '.' + ' ' + tempStr[tempStr.index('/">')+3:tempStr.index('</a>')]
+        numberName = tempStr
+    elif ('<br/>') in tempStr:
+        tempStr = tempStr[8:]
+        numberName = tempStr
+    else:
+        numberName = tempStr[3:-4]
+    tempStr = str(desc[i+1])
+    album = tempStr[6:-8]
+    print(f'{numberName} {album}')
+
