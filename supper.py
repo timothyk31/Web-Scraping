b48c39bb6ba8c5fdb6b6bfeb4b8c157c669b16a0
timothyk31 <timothykurniawan16@gmail.com>
2023-03-28 20:54:59 -0500

    first commit

---
+++ supper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+html_text = requests.get('https://engineering.tamu.edu/cse/profiles/index.html#Faculty').text
+soup = BeautifulSoup(html_text, 'lxml')
+desc = soup.find_all('div', class_ = 'profile')
+results = ''
+print(soup)
